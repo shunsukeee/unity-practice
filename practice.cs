2923a50978ba0bf37015943cf3ae07b7b97716ba
shunsuke <shunsuke.nakayama36@gmail.com>
2021-01-08 13:04:57 +0900

    [add]method

--- practice.cs
+++ practice.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class practice : MonoBehaviour
 {
+
+    int playerHp = 100;
+    int playerAtk = 50;
+
+    int bossHp = 100;
+    int bossAtk = 20;
+
     // Start is called before the first frame update
     void Start()
     {
-        int playerHp = 100;
-        int playerAtk = 50;
-
-        int bossHp = 100;
-        int bossAtk = 20;
 
         Debug.Log("魔王が現れた！");
+        Turn();
+        Turn();
+        Debug.Log("魔王のHPが" + bossHp + "になった。魔王をやっつけた");
+    }
+
+    void Turn()
+    {
         Debug.Log("勇者HP" + playerHp);
         Debug.Log("魔王HP" + bossHp);
 
@@ -22,15 +31,6 @@ public class practice : MonoBehaviour
 
         playerHp -= bossAtk;
         bossHp -= playerAtk;
-
-        Debug.Log("勇者HP" + playerHp);
-        Debug.Log("魔王HP" + bossHp);
-
-        Debug.Log("勇者の攻撃 * 魔王に" + playerAtk + "のダメージを与えた");
-
-        bossHp -= playerAtk;
-
-        Debug.Log("魔王のHPが" + bossHp + "になった。魔王をやっつけた");
     }
 
     // Update is called once per frame
